b6fd38c47a63c8dc39dcae4e338c4d8dcdf39cc5
nbellock <nbellock@gmail.com>
2018-07-31 10:44:53 -0600

    'message'

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+var Sequelize = require('sequelize'),
+    connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    connection = new Sequelize('burgers_db', 'root', 'root', {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: '3000'
+    })
+};
+
+module.exports = connection;
---
+++ migrations/20161113195208-create-menus.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+    up: function (queryInterface, Sequelize) {
+        return queryInterface.createTable('Menus', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            item: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            entree: {
+                allowNull: false,
+                defaultValue: 1,
+                type: Sequelize.BOOLEAN
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: function (queryInterface, Sequelize) {
+        return queryInterface.dropTable('Menus');
+    }
+};
